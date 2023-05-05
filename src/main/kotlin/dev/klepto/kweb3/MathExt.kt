fbb892e2af2a23583b7060410af87a12da10b651
ReverendDread <andrewshilling10@gmail.com>
2023-05-05 16:17:13 -0500

    feat: add uint * uint operator

--- src/main/kotlin/dev/klepto/kweb3/MathExt.kt
+++ src/main/kotlin/dev/klepto/kweb3/MathExt.kt
@@ -12,4 +12,5 @@ operator fun <T : Numeric<*>> T.plus(other: Numeric<*>): T = add(other) as T
 operator fun <T : Numeric<*>> T.minus(other: Numeric<*>): T = sub(other) as T
 operator fun <T : Numeric<*>> T.times(other: Numeric<*>): T = mul(other) as T
 operator fun Uint.times(other: BigInteger): BigInteger = value * other
+operator fun Uint.times(other: Uint): Uint = mul(other)
 operator fun BigInteger.times(other: Uint): BigInteger = this * other.value
